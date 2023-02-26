2ce3b8c70f56878f612b8b176e41c06f65c8e369
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-02-26 19:55:38 +0600

    addeded modal

--- js/script.js
+++ js/script.js
@@ -1,45 +1,48 @@
-const loadPhones = async(searchText,dataLimit)=>{
-    const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+const loadPhones = async (searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data,dataLimit)
-   
+    displayPhones(data.data, dataLimit)
+
 }
 
-const displayPhones = (phones,dataLimit) =>{
+const displayPhones = (phones, dataLimit) => {
 
-    const phoneContainer=document.getElementById('Phone-container');
+    const phoneContainer = document.getElementById('Phone-container');
     phoneContainer.innerText = '';
     const showAll = document.getElementById('show-all');
-    if(dataLimit && phones.length > 10){
-        phones = phones.slice(0,10);
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
         showAll.classList.remove('d-none')
 
-    }else{
+    } else {
         showAll.classList.add('d-none')
     };
 
-    
+
     const noPhone = document.getElementById('no-phone-found');
-    if(phones.length === 0){
+    if (phones.length === 0) {
         noPhone.classList.remove('d-none')
-    }else{
+    } else {
         noPhone.classList.add('d-none')
     }
 
-   
+
     // console.log(phones);
-    phones.forEach(phone =>{
-        // console.log(phone)
-        const phoneDiv =document.createElement('div');
+    phones.forEach(phone => {
+        // console.log(phone.slug)
+        const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
-        phoneDiv.innerHTML =`
+        phoneDiv.innerHTML = `
         
         <div class="card p-4">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailsModal">Go somewhere</button>
+
+          
         </div>
       </div>
         
@@ -48,46 +51,71 @@ const displayPhones = (phones,dataLimit) =>{
         
         `;
         phoneContainer.appendChild(phoneDiv);
-      
-        
+
+
     });
     toggleSpinner(false);
 }
 
 
-document.getElementById('btn-search').addEventListener("click", function(){
+document.getElementById('btn-search').addEventListener("click", function () {
 
-        processSearch(10);
+    processSearch(10);
 });
 
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
 
-const processSearch = (dataLimit) =>{
+
+const processSearch = (dataLimit) => {
     toggleSpinner(true)
-    const searchField =document.getElementById('search-field');
+    const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText)
-    loadPhones(searchText,dataLimit)
+    loadPhones(searchText, dataLimit)
 
 }
 
 
-const toggleSpinner = isLoading =>{
+const toggleSpinner = isLoading => {
     const loadingSection = document.getElementById('loader');
-    if(isLoading){
+    if (isLoading) {
         loadingSection.classList.remove('d-none')
-    }else{
+    } else {
         loadingSection.classList.add('d-none')
     }
 };
 
 
-document.getElementById('btn-show-all').addEventListener('click', function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 
-})
+});
+
+const loadPhoneDetails = async id =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data)
+
+};
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailsModalLabel');
+    modalTitle.innerText = phone.name;
+    const modalImg = document.getElementById('modal-img');
+    modalImg.innerHTML = `
+    <img class="rounded mx-auto d-block" src ="${phone.image}">
+    
+    
+    `;
+}
 
 
 
 
-// loadPhones();
-
+loadPhones('apple');
